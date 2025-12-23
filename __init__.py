304492da94cfda5274d480a010443b1bb1fa766a
Alex Sundukov <86538479+PLUXINDUI@users.noreply.github.com>
2025-12-23 15:57:43 +0300

    Delete __init__.py

--- __init__.py
+++
@@ -1,6 +0,0 @@
-"""Модули для системы мониторинга дисциплины"""
-from .detection import ViolationDetector
-from .face_recognition import FaceRecognizer
-from .video_processor import VideoProcessor
-
-__all__ = ['ViolationDetector', 'FaceRecognizer', 'VideoProcessor']
